fddd5a4a9cdd08b16b234b328c93bab52be7e705
jarek7410 <39216951+jarek7410@users.noreply.github.com>
2024-11-14 13:38:45 +0100

    enw text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import {ColorsTransparent} from "@/assets/ColorsTransparent";
 
 export default function HomeScreen() {
   return (
@@ -20,32 +21,32 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">This application allows you to see the noise on the map</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+          Just change screens to see map with the colors circles!
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
+      <ThemedView style={[styles.stepContainer,{backgroundColor:"#000000",padding:8,borderRadius:10}]}>
+        {/*<ThemedText type="subtitle">Step 2: Explore</ThemedText>*/}
+          <ThemedText style={{color:ColorsTransparent.high}}>
+              red - high noise level
+          </ThemedText>
+          <ThemedText style={{color:ColorsTransparent.medium}}>
+              yellow - medium noise level
+          </ThemedText>
+          <ThemedText style={{color:ColorsTransparent.low}}>
+              green - low noise level
+          </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+            <ThemedText type="subtitle">If u allow it u can send your data too!</ThemedText>
+            {/*<ThemedText>Thank you for using our app!</ThemedText>*/}
+        </ThemedView>
+
+        <ThemedView style={styles.stepContainer}>
+            <ThemedText type="subtitle">Enjoy!</ThemedText>
+            <ThemedText>Thank you for using our app!</ThemedText>
+        </ThemedView>
     </ParallaxScrollView>
   );
 }
